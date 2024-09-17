1a576907833ba15330a1fbe71faf5697660b2fdc
ArchyScript <yungdansa@gmail.com>
2024-09-17 16:58:54 +0100

    ...

--- src/views/Chats.vue
+++ src/views/Chats.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
 import ChatsPage from '../components/Chats/index.vue'
+
 </script>
 
 <template>
-    <div>
-        etst
-        <ChatsPage />
+    <div class="min-h-[100vh] py-80">wewe
+        etstewew ererer
+        <ChatsPage /> wwewe
     </div>
 
 </template>
